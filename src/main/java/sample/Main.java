8e361ea5f982776c0de879a689cac60209689d19
wbenica <wbenica@calpoly.edu>
2018-12-31 16:50:35 -0800

    add employee button adds to db

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,11 +2,14 @@ package sample;
 
 import data.Database;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -36,6 +39,9 @@ public class Main extends Application {
         Button buttAddEmployee = new Button ( "Add Employee" );
 
         GridPane gp = new GridPane ( );
+        gp.setVgap ( 5 );
+        gp.setHgap ( 5 );
+        gp.setPadding ( new Insets ( 20 ) );
         gp.add ( labelFirstName, 0, 0 );
         gp.add ( textFirstName, 1, 0 );
         gp.add ( labelLastName, 0, 1 );
@@ -43,8 +49,24 @@ public class Main extends Application {
         gp.add ( labelPosition, 0, 2 );
         gp.add ( menuPosition, 1, 2 );
         gp.add ( buttAddEmployee, 0, 3, 2, 1 );
+        buttAddEmployee.setOnMouseReleased (
+                new EventHandler<MouseEvent> ( ) {
+
+                    @Override
+                    public void handle ( MouseEvent event ) {
+
+                        String f_name = textFirstName.getText ( );
+                        String l_name = textLastName.getText ( );
+                        String pos    = menuPosition.getValue ( ).toString ( );
+                        Database.addEmployee ( f_name, l_name, pos );
+                        textFirstName.clear ( );
+                        textLastName.clear ( );
+                        menuPosition.setValue ( null );
+                    }
+                } );
 
         Scene scene = new Scene ( gp, 300, 300 );
+        scene.getStylesheets ( ).add ( "styles.css" );
         primaryStage.setTitle ( "Add Employee" );
         primaryStage.setScene ( scene );
         primaryStage.show ();
